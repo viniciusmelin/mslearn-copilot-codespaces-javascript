29c45ac22ad4327f625a9d997a85cfe22591acbb
Vinicius Melin <viniciusti.melin@hotmail.com>
2025-11-08 19:46:19 +0000

    Update siteProps with personal information and add animation to social icons

--- src/App.jsx
+++ src/App.jsx
@@ -24,14 +24,14 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
+  name: "Vinicius Melin",
   title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  email: "vinicius@example.com",
+  gitHub: "viniciusmelin",
+  instagram: "viniciusmelin",
+  linkedIn: "vinicius-melin",
   medium: "",
-  twitter: "microsoft",
+  twitter: "viniciusmelin",
   youTube: "Code",
 };
 
